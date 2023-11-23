eaeeea7d557f0cce2a919ca5213b07af7e907a87
MiguelMercadoS <54323800+MiguelMercadoS@users.noreply.github.com>
2023-11-22 23:59:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="broker.mqttdashboard.com"
+broker="190.158.28.34"
 port=1883
 client1= paho.Client("MiguelMercadoS")
 client1.on_message = on_message
